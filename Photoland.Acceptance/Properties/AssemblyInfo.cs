52d49513846db5311f277a2e58a750bfa1786881
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-12-23 14:48:43 +0600

    1.7.15.1223

--- Photoland.Acceptance/Properties/AssemblyInfo.cs
+++ Photoland.Acceptance/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.7.15.1218")]
-[assembly: AssemblyFileVersion("1.7.15.1218")]
+[assembly: AssemblyVersion("1.7.15.1223")]
+[assembly: AssemblyFileVersion("1.7.15.1223")]
